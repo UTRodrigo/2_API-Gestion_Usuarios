6cc1c114c06dd0acdc55b75c5269aecf4e3f5bc2
Rodrigo Zamacona <rozamco31@gmail.com>
2026-06-24 13:22:01 -0600

    src file created, inside models file and definition of user schema. hiding password field

---
+++ src/models/usuarios.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const usuarioSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false
+    }
+});
+
+module.exports = mongoose.model("Usuario", usuarioSchema);
